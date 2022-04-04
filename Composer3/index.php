3610f5faf565e16e6e34ed388843aa359533b716
DangirasH <97556742+DangirasH@users.noreply.github.com>
2022-04-04 11:15:40 +0200

    Add files via upload

---
+++ Composer3/index.php
@@ -0,0 +1,19 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use CowSay\Cow;
+
+$bessie = new Cow('Hello, Farm!');
+
+$output = $bessie->say();
+echo $output;
+
+echo $bessie;
+
+$bessie = new Cow('Hello, Farm!');
+$bessie->setEyes('oO')
+    ->setTongue('U')
+    ->setPoop('@@@')
+    ->setUdder('W');
+echo $bessie;
